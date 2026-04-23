1c64e0c0b0eddc287a45110f7978e421009d32b6
H2Long <547476809@qq.com>
2026-04-23 23:24:02 +0800

    批量初始化提交

---
+++ build/CMakeFiles/lvgl-sest2.0.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/startup_stm32f407xx.s" "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/build/CMakeFiles/lvgl-sest2.0.dir/startup_stm32f407xx.s.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/cmake/stm32cubemx/../../Core/Inc"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/cmake/stm32cubemx/../../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/cmake/stm32cubemx/../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/cmake/stm32cubemx/../../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/cmake/stm32cubemx/../../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/cmake/stm32cubemx/../../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/cmake/stm32cubemx/../../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/cmake/stm32cubemx/../../Drivers/CMSIS/Include"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/cmake/stm32cubemx/../../Middlewares/LVGL/src"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/cmake/stm32cubemx/../../Middlewares/LVGL"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/cmake/stm32cubemx/../../Core/Src"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/Core/Src/freertos.c" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/freertos.c.o" "gcc" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/freertos.c.o.d"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/Core/Src/fsmc.c" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/fsmc.c.o" "gcc" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/fsmc.c.o.d"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/Core/Src/gpio.c" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/gpio.c.o" "gcc" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/gpio.c.o.d"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/Core/Src/lcd.c" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/lcd.c.o" "gcc" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/lcd.c.o.d"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/Core/Src/main.c" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/main.c.o" "gcc" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/main.c.o.d"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/stm32f4xx_hal_msp.c.o" "gcc" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/stm32f4xx_hal_msp.c.o.d"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/Core/Src/stm32f4xx_hal_timebase_tim.c" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/stm32f4xx_hal_timebase_tim.c.o" "gcc" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/stm32f4xx_hal_timebase_tim.c.o.d"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/Core/Src/stm32f4xx_it.c" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/stm32f4xx_it.c.o" "gcc" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/stm32f4xx_it.c.o.d"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/Core/Src/syscalls.c" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/syscalls.c.o" "gcc" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/syscalls.c.o.d"
+  "/media/hhlong/data/Code Hub/STM32   Hub/HAL    Project/lvgl-sest2.0/Core/Src/sysmem.c" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/sysmem.c.o" "gcc" "CMakeFiles/lvgl-sest2.0.dir/Core/Src/sysmem.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/lvgl-sest2.0.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "lvgl-sest2.0.map"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/freertos.c.o"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/freertos.c.o.d"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/fsmc.c.o"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/fsmc.c.o.d"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/gpio.c.o"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/gpio.c.o.d"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/lcd.c.o"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/lcd.c.o.d"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/main.c.o"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/main.c.o.d"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/stm32f4xx_hal_msp.c.o"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/stm32f4xx_hal_msp.c.o.d"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/stm32f4xx_hal_timebase_tim.c.o"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/stm32f4xx_hal_timebase_tim.c.o.d"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/stm32f4xx_it.c.o"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/stm32f4xx_it.c.o.d"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/syscalls.c.o"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/syscalls.c.o.d"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/sysmem.c.o"
+  "CMakeFiles/lvgl-sest2.0.dir/Core/Src/sysmem.c.o.d"
+  "CMakeFiles/lvgl-sest2.0.dir/startup_stm32f407xx.s.o"
+  "lvgl-sest2.0"
+  "lvgl-sest2.0.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/lvgl-sest2.0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
